d107f13d5de73be834cf475e7a5ad98cd10a4468
Nagibator1337-xakep <pavel.beloff@gmail.com>
2022-03-23 13:57:29 +0500

    Working prototype

--- src/main/java/UpdateDownloader_Main.java
+++ src/main/java/UpdateDownloader_Main.java
@@ -1,26 +1,24 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class UpdateDownloader_Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String userHomeDir = System.getProperty("user.home");
         String filePath = userHomeDir +File.separator+ "gar" +File.separator+ "gar_delta_xml.zip";
+        String dirPath = userHomeDir +File.separator+ "gar" +File.separator+ "gar_delta_xml";
 
         String FIAS_JSON_REQUEST_URL = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
         String UPDATE_JSON_KEY = "GarXMLDeltaURL";
 
-//        ActualGarDeltaDownloader downloader = new ActualGarDeltaDownloader();
-//        downloader.setFilePath(FILE_PATH);
-//        downloader.setUrlPath(URL_PATH);
-
-//        downloader.download();
-
-//        UnzipFile unzipFile = new UnzipFile(FILE_PATH);
-//        unzipFile.unzip();
-
         System.out.println("Добро пожаловать в менеджер загрузки обновлений ГАР БД ФИАС");
         System.out.println("Для начала загрузки обновления нажмите Enter...");
         pressEnterToContinue();
+
+        System.out.println("Загрузка осуществляется в папку: " + userHomeDir +File.separator+ "gar");
 
         String garUpdateLink = JsonProcessing.getUpdateLink(FIAS_JSON_REQUEST_URL, UPDATE_JSON_KEY);
 
@@ -36,7 +34,46 @@ public class UpdateDownloader_Main {
         UnzipFile unzipFile = new UnzipFile(filePath);
         unzipFile.unzip();
 
+        File directoryPath = new File(dirPath);
 
+        List<File> files = new ArrayList<>();
+        File[] filesList = directoryPath.listFiles();
+        for(File file : Objects.requireNonNull(filesList)) {
+            if (file.isDirectory()) {
+                File subdirFile = new File(file.getAbsolutePath());
+                File[] subList = subdirFile.listFiles();
+                files.addAll(Arrays.asList(Objects.requireNonNull(subList)));
+            } else {
+                files.add(file);
+            }
+        }
+
+        System.out.println("Для начала преобразования файлов XML в CSV нажмите Enter...");
+        pressEnterToContinue();
+
+        int howManyFiles = files.size();
+        int filesCounter = 1;
+        for (File file : files) {
+            XmlToCsvConverter xmlToCsvConverter = new XmlToCsvConverter();
+            xmlToCsvConverter.readXml(file.getAbsolutePath());
+
+
+            int garIndex = dirPath.length();
+            String csvStr = dirPath.substring(0,garIndex-14)
+                    +File.separator+ "csv" +File.separator+
+                    file.getAbsolutePath().substring(garIndex+1,file.getAbsolutePath().length()-4)
+                    +".csv";
+
+            System.out.print(" | Преобразование файла: "+file.getName().substring(0,20)
+                    +"....CSV ("+filesCounter+" из "+howManyFiles+")");
+            System.out.print("                                           \r");
+
+            xmlToCsvConverter.buildCSV(csvStr);
+            filesCounter++;
+        }
+        System.out.println("\nПреобразование завершено, хорошего дня!");
+        System.out.println("Для выхода из программы нажмите Enter");
+        pressEnterToContinue();
     }
 
     private static void pressEnterToContinue()
--- src/main/java/XmlToCsvConverter.java
+++ src/main/java/XmlToCsvConverter.java
@@ -1,3 +1,151 @@
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import org.xml.sax.InputSource;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class XmlToCsvConverter {
+
+    private final List<List<String>> dataRowsNames = new ArrayList<>();
+    private final List<List<String>> dataRowsValues = new ArrayList<>();
+    private String xmlString = "";
+    private String docRootElement = "";
+    private final Pattern pattern = Pattern.compile("[^\\w-:.]*");
+
+
+    public XmlToCsvConverter() {
+    }
+
+    public void readXml(String filePath) throws IOException {
+        Path fileName = Path.of(filePath);
+        byte[] content = Files.readAllBytes(fileName);
+
+        String xmlString = new String(content, StandardCharsets.UTF_8);
+
+        xmlString = xmlString.replaceFirst("^\uFEFF", "");
+
+        this.xmlString = xmlString;
+
+        Document doc = null;
+        try {
+
+            StringReader sr = new StringReader(xmlString);
+            InputSource is = new InputSource(sr);
+
+            SAXBuilder saxBuilder = new SAXBuilder();
+            doc = saxBuilder.build(is);
+            Element classElement = doc.getRootElement();
+
+            List<Element> elementsList = classElement.getChildren();
+
+            elementsList.forEach(element -> {
+                List<Attribute> attributeList = element.getAttributes();
+
+                List<String> names = new ArrayList<>();
+                List<String> values = new ArrayList<>();
+
+                attributeList.forEach(attribute -> {
+                    names.add(attribute.getName());
+                    values.add(attribute.getValue());
+                });
+                dataRowsNames.add(names);
+                dataRowsValues.add(values);
+            });
+
+            normalizeData();
+
+        } catch (IOException | JDOMException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка обработки файла XML");
+        }
+
+        docRootElement = Objects.requireNonNull(doc).getRootElement().getName();
+    }
+
+    public void buildCSV(String strPath) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        if (!dataRowsNames.isEmpty()) {
+            List<String> header = dataRowsNames.get(0);
+
+            for (int i = 0; i < header.size(); i++) {
+                sb.append(header.get(i));
+                if (i != header.size() - 1) sb.append(',');
+            }
+
+            sb.append("\r\n");
+
+            for (int j = 0; j < dataRowsValues.size(); j++) {
+                List<String> row = dataRowsValues.get(j);
+                for (int i = 0; i < row.size(); i++) {
+                    String element = row.get(i);
+                    if (isTextString(element)) {
+                        sb.append("\"").append(element).append("\"");
+                    } else sb.append(element);
+                    if (i != row.size() - 1) sb.append(',');
+                }
+                if (j != dataRowsValues.size() - 1) sb.append("\r\n");
+            }
+        } else sb.append(docRootElement);
+
+        Path path = Paths.get(strPath);
+        new File(path.getParent().toString()).mkdirs();
+        Files.writeString(path, sb.toString());
+    }
+
+    public String getXmlString() {
+        return xmlString;
+    }
+
+    private boolean isTextString(String str) {
+        if (str == null) {
+            return false;
+        }
+        return pattern.matcher(str).matches();
+    }
+
+    private void normalizeData() {
+        if (!dataRowsNames.isEmpty()) {
+            int size = dataRowsNames.get(0).size();
+            List<Integer> indexes = new ArrayList<>();
+
+            for (int i = 1; i < dataRowsNames.size(); i++) {
+                if (dataRowsNames.get(i).size() != size && dataRowsNames.get(i).size() < size) {
+                    indexes.add(i);
+                }
+            }
+
+            if (!indexes.isEmpty()) {
+                for (Integer integer : indexes) {
+                    List<String> rowName = dataRowsNames.get(integer);
+                    List<String> rowValue = dataRowsValues.get(integer);
+                    for (int i = 0; i < rowName.size(); i++) {
+                        String string = rowName.get(i);
+                        if (!Objects.equals(dataRowsNames.get(0).get(i), string)) {
+                            rowName.add(i, dataRowsNames.get(0).get(i));
+                            rowValue.add(i, "null");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 
 }
